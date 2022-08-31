2034399236626acb356d77d5d7a83527f0171a3f
jarltuxen <jarl.tuxen@gmail.com>
2022-08-31 10:30:17 +0200

    Initial commit

---
+++ src/main/java/dk/kea/hellospringdemo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package dk.kea.hellospringdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hellospring")
+    public String helloSpring(){
+        return "Hello Spring!";
+    }
+
+    @GetMapping()
+    public String defaultPage(){
+        return "Godmorgen!";
+    }
+}
---
+++ src/test/java/dk/kea/hellospringdemo/HellospringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.hellospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
