9853e3437f84876da4d9c8b877e0abb1fbb4d1c8
jarltuxen <jarl.tuxen@gmail.com>
2022-08-31 11:00:52 +0200

    parameter-url

--- src/main/java/dk/kea/hellospringdemo/controller/HelloController.java
+++ src/main/java/dk/kea/hellospringdemo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package dk.kea.hellospringdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,9 @@ public class HelloController {
         return "Godmorgen!";
     }
 
+    @GetMapping("/parametertest")
+    public String parameter(@RequestParam String mintekst, @RequestParam String minandentekst){
+        return mintekst + " " + minandentekst;
+    }
 
 }
